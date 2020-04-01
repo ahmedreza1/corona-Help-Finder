289d5091c23de84c6e04a71d535f012b278d7202
Ahmed Reza <ahmedreza80@gmail.com>
2020-04-01 17:35:53 +0530

    Installed django-User_agents and added mobile view in dashboard

--- crona/settings.py
+++ crona/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'covid',
+    'django_user_agents',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_user_agents.middleware.UserAgentMiddleware',
 ]
 
 ROOT_URLCONF = 'crona.urls'
