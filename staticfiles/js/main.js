763011a14b4d2ebee466d0e135749b9381e863f1
Ahmed Reza <ahmedreza80@gmail.com>
2020-05-03 01:25:00 +0530

    pushing the changes to the server

---
+++ staticfiles/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+// SideNav Initialization
+$(".button-collapse").sideNav();
+
+new WOW().init();
+
+
