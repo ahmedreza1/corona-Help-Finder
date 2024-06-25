0de481420f52ed32586c8e0b4d7c8a812856dc12
Md Ahmed Reza Siddique <ahmedreza80@gmail.com>
2024-06-25 21:26:53 +0530

    Update views.py

--- covid/views.py
+++ covid/views.py
@@ -41,19 +41,16 @@ class DashboardView(TemplateView):
         request.user_agent.is_touch_capable # returns True
         request.user_agent.is_pc # returns False
         request.user_agent.is_bot # returns False
-
         # Accessing user agent's browser attributes
         request.user_agent.browser  # returns Browser(family=u'Mobile Safari', version=(5, 1), version_string='5.1')
         request.user_agent.browser.family  # returns 'Mobile Safari'
         request.user_agent.browser.version  # returns (5, 1)
         request.user_agent.browser.version_string   # returns '5.1'
-
         # Operating System properties
         request.user_agent.os  # returns OperatingSystem(family=u'iOS', version=(5, 1), version_string='5.1')
         request.user_agent.os.family  # returns 'iOS'
         request.user_agent.os.version  # returns (5, 1)
         request.user_agent.os.version_string  # returns '5.1'
-
         # Device properties
         request.user_agent.device  # returns Device(family='iPhone')
         request.user_agent.device.family  # returns 'iPhone'
