eecbdd6e040a54074fb5f6b2b499b0f3e9b8a9c6
Md Ahmed Reza Siddique <ahmedreza80@gmail.com>
2024-06-25 21:26:22 +0530

    Update urls.py

--- covid/urls.py
+++ covid/urls.py
@@ -10,23 +10,29 @@ urlpatterns = [
     path('tips/', views.TipsViews.as_view()),
     path('info/', views.InfoView.as_view()),
     path('dashboard', views.DashboardView.as_view()),
+    
 # Choose State URL
     path('chose_state', views.chose_state, name='chose_state'),
+    
 # Orginisations Profiles
     path('profile/edit/<int:pk>', views.MyProfileUpdateView.as_view(success_url="/covid/home")),
     path('myprofile/', views.MyProfileListView.as_view()),
     path('myprofile/<int:pk>', views.MyProfileDetailView.as_view()),
+    
 # Work/Post URL
     path('mypost/create/', views.MyPostCreate.as_view(success_url="/covid/mypost")),
     path('mypost/delete/<int:pk>', views.MyPostDeleteView.as_view(success_url="/covid/mypost")),
     path('mypost/', views.MyPostListView.as_view()),
     path('mypost/<int:pk>', views.MyPostDetailView.as_view()),
     path('mypost/edit/<int:pk>', views.MyPostUpdateView.as_view(success_url="/covid/mypost")),
+    
 # Orginizations Bank Account Details URL
     path('mypayment/create/', views.MyPaymentCreate.as_view(success_url="/covid/myprofile")),
     path('mypayment/edit/<int:pk>', views.MyPaymentUpdateView.as_view(success_url="/covid/myprofile")),
+    
 # FakeCheck URL
     path('fakecheck/', views.FakecheckListView.as_view()),
+    
 # Root URL
     path('', RedirectView.as_view(url="home/")), 
 ]
